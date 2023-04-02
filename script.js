c489cb4070d93386357786ac5c6b298999add983
donnebiyu0 <nebiyuab00@gmail.com>
2023-04-02 11:22:54 -0700

    first demo

---
+++ script.js
@@ -0,0 +1,72 @@
+const toggleButton = document.getElementById('toggle-button');
+let roundBg = document.querySelector('.round-toggle')
+const switchT = document.querySelector('input[type="checkbox"]')
+
+function switchMOde(event) {
+    if(event.target.checked){
+        document.documentElement.setAttribute('data-theme', 'dark')
+        roundBg.classList.add('change')
+    }
+    else{
+        document.documentElement.setAttribute('data-theme', 'light')
+        roundBg.classList.remove('change')
+    }
+}
+
+switchT.addEventListener('change', switchMOde)
+
+// swipper stle
+var swipers = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+// end of swipper style
+
+// service swiper
+var swiper = new Swiper(".mySwiper-service", {
+      cssMode: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      mousewheel: true,
+      keyboard: true,
+    });
+// end of service swiper
+
+let wrappers = document.querySelectorAll('.wrapper')
+let menuBtn = document.querySelector('.menu-wrapper')
+let navBar = document.querySelector('.nav-bar')
+menuBtn.addEventListener('click', ()=>{
+  menuBtn.classList.toggle('change');
+  navBar.classList.toggle('change')
+  wrappers.forEach((wrapper)=>{
+    wrapper.classList.toggle('target')
+  } )
+})
+wrappers.forEach((wrapper)=>{
+  wrapper.addEventListener('click', ()=>{
+    menuBtn.classList.remove('change')
+    navBar.classList.remove('change')
+    wrappers.forEach((element) =>{
+      element.classList.remove('target')
+    })
+  })
+})
+
+
